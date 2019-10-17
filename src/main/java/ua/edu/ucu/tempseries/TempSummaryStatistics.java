6053233349d8be05a68213c16caf0eafd16fd353
matrunych <47101257+matrunych@users.noreply.github.com>
2019-10-17 08:40:09 +0300

    Update TempSummaryStatistics.java

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -7,7 +7,7 @@ public class TempSummaryStatistics {
     private double maxTemp;
 
 
-    public TempSummaryStatistics(double avgTemp, double devTemp, double minTemp, double maxTemp){
+    public TempSummaryStatistics(double avgTemp, double devTemp, double minTemp, double maxTemp) {
         this.avgTemp = avgTemp;
         this.devTemp = devTemp;
         this.minTemp = minTemp;
